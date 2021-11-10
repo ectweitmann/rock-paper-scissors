523290cda44d962cecf779a8dd7593ac911d68f2
Ethan Tweitmann <ethantweitmann@gmail.com>
2021-11-10 14:40:12 -0800

    Create shell of Player class

---
+++ src/player.js
@@ -0,0 +1,19 @@
+class Player {
+  constructor(name, token, wins) {
+    this.name = name;
+    this.token = token;
+    this.wins = wins;
+  }
+
+  saveWinsToStorage() {
+
+  }
+
+  retrieveWinsFromStorage() {
+
+  }
+
+  takeTurn() {
+    
+  }
+}
