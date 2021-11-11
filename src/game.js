3f850da97a01318cf512ec413533eb37892adab3
Ethan Tweitmann <ethantweitmann@gmail.com>
2021-11-10 19:47:31 -0800

    Add basic components to Game class

---
+++ src/game.js
@@ -0,0 +1,27 @@
+class Game {
+  constructor (player1, player2, version) {
+    this.player1 = player1;
+    this.player2 = player2;
+    this.version = version;
+  }
+
+  playGame(version) {
+
+  }
+
+  compareChampions(version) {
+
+  }
+
+  selectChampion(player, champion) {
+
+  }
+
+  addWin(player) {
+
+  }
+
+  reset() {
+
+  }
+}
