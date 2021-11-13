98ceb277b0b43c6015cc901071916082a2bae7bb
Ethan Tweitmann <ethantweitmann@gmail.com>
2021-11-12 17:11:15 -0800

    Remove original/alternate Champion objects and add gameLogic property to class

--- src/game.js
+++ src/game.js
@@ -1,8 +1,8 @@
 class Game {
-  constructor (player1, player2, version) {
+  constructor (player1, player2, type) {
     this.player1 = player1;
     this.player2 = player2;
-    this.version = version;
+    this.gameLogic = gameLogic[type];
   }
 
   playGame(version) {
@@ -53,51 +53,3 @@ class Game {
 
   }
 }
-
-var originalChampions = {
-  rock: {
-    beatsPaper: false,
-    beatsScissors: true,
-  },
-  paper: {
-    beatsScissors: false,
-    beatsRock: true,
-  },
-  scissors: {
-    beatsRock: false,
-    beatsPaper: true,
-  },
-};
-
-var alternateChampions = {
-  rock: {
-    beatsPaper: false,
-    beatsSpock: false,
-    beatsLizard: true,
-    beatsScissors: true,
-  },
-  paper: {
-    beatsScissors: false,
-    beatsLizard: false,
-    beatsSpock: true,
-    beatsRock: true,
-  },
-  scissors: {
-    beatsRock: false,
-    beatsSpock: false,
-    beatsLizard: true,
-    beatsPaper: true,
-  },
-  lizard: {
-    beatsRock: false,
-    beatsScissors: false,
-    beatsPaper: true,
-    beatsSpock: true,
-  },
-  spock: {
-    beatsPaper: false,
-    beatsLizard: false,
-    beatsScissors: true,
-    beatsRock: true,
-  },
-};
