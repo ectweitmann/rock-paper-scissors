7c7f22b63f9e3876401bb0dda0bb52b4641f0ccd
Ethan Tweitmann <ethantweitmann@gmail.com>
2021-11-15 11:10:48 -0800

    Update invocation of takeTurn method in selectChampions function for computer

--- src/main.js
+++ src/main.js
@@ -36,7 +36,7 @@ function selectChampions(event) {
   currentGame.player1.takeTurn(event.target.id);
   currentGame.player1.championToken = event.target;
   toggleElementOrder(event.target);
-  currentGame.player2.takeTurn(currentGame.gameLogic.champions[getRandomIndex(currentGame.gameLogic.champions)]);
+  currentGame.player2.takeTurn();
   currentGame.player2.championToken = `assets/${currentGame.player2.champion}.png`;
 }
 
