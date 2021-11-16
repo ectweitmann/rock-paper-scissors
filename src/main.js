ddb3514a919356bee80a8d2e9399a611d2c7ae1c
Ethan Tweitmann <ethantweitmann@gmail.com>
2021-11-16 10:26:09 -0800

    Organize file layout to improve readability

--- src/main.js
+++ src/main.js
@@ -1,49 +1,104 @@
+// Query Selectors
+// Players
+var player1Token = document.querySelector('#player1Token').src;
 var player1Name = document.querySelector('#player1Name');
 var player1Wins = document.querySelector('#player1Wins');
 var player1ChampionSelection = document.querySelector('#p1ChampionSelection');
+var player2Token = document.querySelector('#player2Token').src;
 var player2Name = document.querySelector('#player2Name');
 var player2Wins = document.querySelector('#player2Wins');
 var player2ChampionSelection = document.querySelector('#p2ChampionSelection');
 
+// Game Types
 var gameTypeContainer = document.querySelector('#gameTypeContainer');
 var classicGameType = document.querySelector('#classicGameType');
 var difficultGameType = document.querySelector('#difficultGameType');
+
+// Champions
 var championIconsContainer= document.querySelector('#championIconsContainer');
+var championIcons = championIconsContainer.querySelectorAll('img');
+
+// Game Taglines
 var gameInstructions = document.querySelector('#gameInstructions');
 var gameResults = document.querySelector('#gameResults');
 
-var player1Token = document.querySelector('#player1Token').src;
-var player2Token = document.querySelector('#player2Token').src;
-var championIcons = championIconsContainer.querySelectorAll('img');
-
+//Buttons
 var buttonChangeGame = document.querySelector('#changeGame');
 var buttonResetScore = document.querySelector('#resetScore');
 
+// currentGame Variable
 var currentGame = new Game(
   new Player(player1Name.innerText, player1Token, player1Wins.innerText),
   new Player(player2Name.innerText, player2Token, player2Wins.innerText)
 );
 
+// Event Listeners
 window.addEventListener('load', setUpGame);
 classicGameType.addEventListener('click', displayGameBoard);
 difficultGameType.addEventListener('click', displayGameBoard);
 buttonChangeGame.addEventListener('click', displayGameMenu);
 buttonResetScore.addEventListener('click', resetScore);
 
-function getGameType(event) {
-  return currentGame.type = event.target.children[0].id;
-}
-
-function applyGameType() {
-  currentGame.establishGameType();
-}
-
+// Callback Functions
 function setUpGame(event) {
   displayPlayerWins();
   addEventListenersToChampionIcons();
   if (player1Wins.innerText !== '0' || player1Wins.innerText !== '0') {
     toggleElementVisibility(buttonResetScore, false);
   }
+}
+
+function playGame(event) {
+  selectChampions(event);
+  displayPlayerChampions();
+  declareGameResult(currentGame.compareChampions());
+  toggleUnselectedChampionsVisibility();
+  setTimeout(resetGameBoard, 900);
+}
+
+function displayGameBoard(event) {
+  toggleGameTypeContainerVisibility();
+  toggleChampionIconsVisibility(getGameType(event));
+  changeGameInstructionText();
+  applyGameType(event);
+}
+
+function displayGameMenu() {
+  toggleChampionIconsVisibility();
+  toggleGameTypeContainerVisibility();
+  changeGameInstructionText();
+  toggleElementVisibility(buttonChangeGame);
+}
+
+function resetScore() {
+  if (player1Wins.innerText !== '0' || player2Wins.innerText !== '0') {
+    localStorage.clear()
+    updatePlayerWins('Reset');
+  }
+  toggleElementVisibility(buttonResetScore);
+}
+
+function resetGameBoard() {
+  toggleElementOrder(currentGame.player1.championToken);
+  for (var i = 0; i < currentGame.gameLogic.champions.length; i++) {
+    toggleElementVisibility(championIcons[i], false);
+    toggleElementClickability(championIcons[i]);
+  }
+  toggleGameTaglines();
+  changeGameInstructionText();
+  toggleElementVisibility(buttonChangeGame, false);
+  toggleElementVisibility(buttonResetScore, player1Wins.innerText === '0' && player2Wins.innerText === '0');
+  displayPlayerChampions();
+  currentGame.reset();
+}
+
+function getGameType(event) {
+  return currentGame.type = event.target.children[0].id;
+}
+
+// Helper Functions
+function applyGameType() {
+  currentGame.establishGameType();
 }
 
 function displayPlayerWins() {
@@ -65,14 +120,6 @@ function displayPlayerChampions() {
   toggleElementVisibility(p2ChampionSelection);
 }
 
-function playGame(event) {
-  selectChampions(event);
-  displayPlayerChampions();
-  declareGameResult(currentGame.compareChampions());
-  toggleUnselectedChampionsVisibility();
-  setTimeout(resetGameBoard, 900);
-}
-
 function declareGameResult(winner) {
   toggleGameTaglines();
   gameResults.innerHTML = '';
@@ -81,7 +128,8 @@ function declareGameResult(winner) {
   }
   gameResults.innerHTML = `
     <p class="game-tagline">
-    <img class="game-result" src="assets/${winner.name}.png"> ${winner.name} won this round!
+    <img class="game-result" src="assets/${winner.name}.png">
+    ${winner.name} won this round!
     <img class="game-result" src="assets/${winner.name}.png">
     </p>`;
   updatePlayerWins(winner);
@@ -167,34 +215,6 @@ function addEventListenersToChampionIcons() {
   for (var i = 0; i < championIcons.length; i++) {
     championIcons[i].addEventListener('click', playGame);
   }
-}
-
-function displayGameBoard(event) {
-  toggleGameTypeContainerVisibility();
-  toggleChampionIconsVisibility(getGameType(event));
-  changeGameInstructionText();
-  applyGameType(event);
-}
-
-function resetGameBoard() {
-  toggleElementOrder(currentGame.player1.championToken);
-  for (var i = 0; i < currentGame.gameLogic.champions.length; i++) {
-    toggleElementVisibility(championIcons[i], false);
-    toggleElementClickability(championIcons[i]);
-  }
-  toggleGameTaglines();
-  changeGameInstructionText();
-  toggleElementVisibility(buttonChangeGame, false);
-  toggleElementVisibility(buttonResetScore, player1Wins.innerText === '0' && player2Wins.innerText === '0');
-  displayPlayerChampions();
-  currentGame.reset();
-}
-
-function displayGameMenu() {
-  toggleChampionIconsVisibility();
-  toggleGameTypeContainerVisibility();
-  changeGameInstructionText();
-  toggleElementVisibility(buttonChangeGame);
 }
 
 function getRandomIndex(array) {
